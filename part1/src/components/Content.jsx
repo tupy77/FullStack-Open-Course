e427a7acc2cd9c9b5aeb9f91d5348e0aafc68b3c
Frank PC <tupy77@hotmail.com>
2024-02-23 10:18:09 +0100

    Part 1 Exercise 1.3-1.5

--- part1/src/components/Content.jsx
+++ part1/src/components/Content.jsx
@@ -1,7 +1,10 @@
 const Content = (props) => {
+    console.log(props)
     return (
         <div>
-            <p>{props.part} {props.exercise}</p>
+            <p>{props.part[0].name} {props.part[0].exercises}</p>
+            <p>{props.part[1].name} {props.part[1].exercises}</p>
+            <p>{props.part[2].name} {props.part[2].exercises}</p>
         </div>
     )
 }
